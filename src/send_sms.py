0e16c255af54d7db671aed1e1f873da3be950577
sebastianpd1 <sebastianpd@me.com>
2019-08-12 17:36:49 +0000

    first commit

---
+++ src/send_sms.py
@@ -0,0 +1,18 @@
+# Download the helper library from https://www.twilio.com/docs/python/install
+from twilio.rest import Client
+
+
+# Your Account Sid and Auth Token from twilio.com/console
+# DANGER! This is insecure. See http://twil.io/secure
+account_sid = 'AC050daa6410a7d71aa3266c8bd22cbdfa'
+auth_token = '821ccca07308bd4f91233471c921e8ed'
+client = Client(account_sid, auth_token)
+
+message = client.messages \
+                .create(
+                     body="Join Earth's mightiest heroes. Like Kevin Bacon.",
+                     from_='+19384440856',
+                     to='+17865577374'
+                 )
+
+print(message.sid)
